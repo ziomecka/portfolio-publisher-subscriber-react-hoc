181189d93365ea73084eed02e0cf957ead602e3c
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-08 22:42:58 +0200

    Src | adds HOC

---
+++ src/hoc.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {
+  Consumer,
+  defaultProps
+} from './context';
+import { EventCallback } from 'publisher-subscriber-pattern';
+
+export interface WithPublisherProps {
+  eventSubscribersCount(eventName: string): number;
+  subscribe(eventName: string, eventCallback: EventCallback): () => void;
+  subscribersCount(): number;
+  unsubscribeAll(): void;
+};
+
+export const withPublisher = <P extends object>(Component: React.ComponentType<P>) => (
+  (props: P & WithPublisherProps): JSX.Element => {
+    return (
+      <Consumer>{({
+        eventSubscribersCount,
+        subscribe,
+        subscribersCount,
+        unsubscribeAll
+      } = defaultProps): JSX.Element => (
+        <Component
+          { ...props }
+          {...{ eventSubscribersCount, subscribe, subscribersCount, unsubscribeAll }}
+        />
+      )}
+      </Consumer>
+    );
+  }
+);
